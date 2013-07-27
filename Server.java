500d1ec0b3996ba17343ded1447f1570e9512988
Lalit Kapoor <lalitkapoor@gmail.com>
2013-07-27 05:37:07 -0500

    clarifying

--- Server.java
+++ Server.java
@@ -18,11 +18,11 @@ public class Server extends Verticle {
   + "<body><h1>Email Signature Detection</h1>"
   + "<a target='_blank' href='https://www.github.com/lalitkapoor/email-signature-detection-server'>github</a>"
   + "<form method='POST'>"
-  + "<textarea name='demobox' rows=40 cols=80 placeholder='paste email in here'></textarea>"
+  + "<textarea name='demobox' rows=40 cols=80 placeholder='paste email contents in here'></textarea>"
   + "<br /><input type='submit' />"
   + "</form></body>"
   + "</html>";
-  
+
   public void start() {
     vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
       public void handle(final HttpServerRequest req) {
