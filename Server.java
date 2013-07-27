c9f9a7692b62d47849105f1359a4256f70719b29
Lalit Kapoor <lalitkapoor@gmail.com>
2013-07-27 04:27:53 -0500

    use spaces not tabs

--- Server.java
+++ Server.java
@@ -14,18 +14,18 @@ public class Server extends Verticle {
     vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
       public void handle(final HttpServerRequest req) {
         req.bodyHandler(new Handler<Buffer>() {
-			public void handle(Buffer buffer) {
-				String email = buffer.toString();
-				SigFilePredictor sigpred = new SigFilePredictor();
-				JSONObject json = new JSONObject();
-				String message = sigpred.getMsgWithoutSignatureLines(email);
-				String signature = sigpred.getSignatureLines(email);
-				json.put("message", message);
-				json.put("signature", signature);
-				req.response().headers().set("Content-Type", "application/json; charset=UTF-8");
-				req.response().end(json.toJSONString());
-			}
-		});
+          public void handle(Buffer buffer) {
+            String email = buffer.toString();
+            SigFilePredictor sigpred = new SigFilePredictor();
+            JSONObject json = new JSONObject();
+            String message = sigpred.getMsgWithoutSignatureLines(email);
+            String signature = sigpred.getSignatureLines(email);
+            json.put("message", message);
+            json.put("signature", signature);
+            req.response().headers().set("Content-Type", "application/json; charset=UTF-8");
+            req.response().end(json.toJSONString());
+          }
+        });
       }
     }).listen(9092);
   }
