602f041cbcbbad1ab2e4c2e0f117548809c5ba69
Lalit Kapoor <lalitkapoor@gmail.com>
2013-07-27 04:22:42 -0500

    initial commit

---
+++ Server.java
@@ -0,0 +1,32 @@
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.platform.Verticle;
+import org.vertx.java.core.buffer.Buffer;
+
+import jangada.SigFilePredictor;
+import org.json.simple.JSONObject;
+
+import java.util.Map;
+
+public class Server extends Verticle {
+
+  public void start() {
+    vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+      public void handle(final HttpServerRequest req) {
+        req.bodyHandler(new Handler<Buffer>() {
+			public void handle(Buffer buffer) {
+				String email = buffer.toString();
+				SigFilePredictor sigpred = new SigFilePredictor();
+				JSONObject json = new JSONObject();
+				String message = sigpred.getMsgWithoutSignatureLines(email);
+				String signature = sigpred.getSignatureLines(email);
+				json.put("message", message);
+				json.put("signature", signature);
+				req.response().headers().set("Content-Type", "application/json; charset=UTF-8");
+				req.response().end(json.toJSONString());
+			}
+		});
+      }
+    }).listen(9092);
+  }
+}
---
+++ run.sh
@@ -0,0 +1,1 @@
+vertx run Server.java -cp "lib/json-simple-1.1.1.jar:lib/jangada.jar:lib/minorThird.jar"
