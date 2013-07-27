9843c75bc5fdf73bd632da712c50c6212fbff7f6
Lalit Kapoor <lalitkapoor@gmail.com>
2013-07-27 05:26:31 -0500

    created simple form to display if GET request

--- Server.java
+++ Server.java
@@ -7,15 +7,37 @@ import jangada.SigFilePredictor;
 import org.json.simple.JSONObject;
 
 import java.util.Map;
+import java.net.URLDecoder;
+import java.io.UnsupportedEncodingException;
+
 
 public class Server extends Verticle {
 
+  public static String page = "<html>"
+  + "<head></head>"
+  + "<body><h1>Email Signature Detection</h1><form method='POST'>"
+  + "<textarea name='demobox' rows=40 cols=80 placeholder='paste email in here'></textarea>"
+  + "<br /><input type='submit' />"
+  + "</form></body>"
+  + "</html>";
   public void start() {
     vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
       public void handle(final HttpServerRequest req) {
+        if (req.method().compareTo("POST") != 0) {
+          req.response().headers().set("Content-Type", "text/html; charset=UTF-8");
+          req.response().end(page);
+          return;
+        }
+
         req.bodyHandler(new Handler<Buffer>() {
           public void handle(Buffer buffer) {
             String email = buffer.toString();
+            try {
+              if(email.indexOf("demobox=") == 0) email = URLDecoder.decode(email.substring(8), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+              req.response().headers().set("Content-Type", "application/json; charset=UTF-8");
+              req.response().end("");
+            }
             SigFilePredictor sigpred = new SigFilePredictor();
             JSONObject json = new JSONObject();
             String message = sigpred.getMsgWithoutSignatureLines(email);
